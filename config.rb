97d43f1eb47d44bfa33a6b9e61819cc78f2768b0
yatsuzuka-nbdc <yatsuzuka@biosciencedbc.jp>
2020-03-27 18:25:12 +0900

    https://github.com/penqe/nbdc-rdf-portal より2020/1/20最終更新のものをアップロード

---
+++ config.rb
@@ -0,0 +1,20 @@
+require 'compass/import-once/activate'
+# Require any additional compass plugins here.
+
+# Set this to the root of your project when deployed:
+http_path = "/"
+css_dir = "build/css"
+sass_dir = "source/css"
+images_dir = "build/images"
+javascripts_dir = "build/js"
+fonts_dir = "build/fonts"
+
+output_style = :expanded
+
+# To enable relative paths to assets via compass helper functions. Uncomment:
+# relative_assets = true
+
+line_comments = false
+color_output = false
+
+preferred_syntax = :sass
